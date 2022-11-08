eaf090d7b29f3b9fc1d5b1ef30340e2f67b4a018
Frommflin <freakyfrommelin@outlook.com>
2022-11-08 11:35:37 +0100

    Added classes for company hardware assets

---
+++ W45_Mini1/Hardware.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W45_Mini1
+{
+    internal class Hardware
+    {
+        public string Type { get; set; } // Stationary Computer, Laptop, Phone etc.
+        public string Brand { get; set; }  // Lenovo, Apple etc.
+        public string Model { get; set; } // iPhone 10, S22 etc.
+        public int Price { get; set; } // In USD? Decide!!!!!
+        public DateTime DateOfPurchase { get; set; }
+    }
+}
---
+++ W45_Mini1/Laptop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W45_Mini1
+{
+    internal class Laptop : Hardware
+    {
+        public Laptop(string type, string brand, string model, int price, DateTime purchased)
+        {
+            Type = type;
+            Brand = brand;
+            Model = model;
+            Price = price;
+            DateOfPurchase = purchased;
+        }
+    }
+}
